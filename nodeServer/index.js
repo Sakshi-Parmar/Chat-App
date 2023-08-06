0a2c4c64a9a2db28972f4c783d899b2c191fe6fc
Sakshi Parmar <sakshiparmarr123@gmail.com>
2023-08-06 20:05:07 +0530

    changes

---
+++ nodeServer/index.js
@@ -0,0 +1,17 @@
+//node server, which will handle socket io connections
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', username => {
+        console.log("New user", username)
+        users[socket.id] = username;
+        socket.broadcast.emit('user-joined', username)
+    });
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: user[socket.id] });
+    });
+
+}) 
